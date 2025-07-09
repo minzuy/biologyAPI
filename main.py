fe8af70740fa7e9f491696017477b7358d683cff
minzuy <duybmse18042@fpt.edu.vn>
2025-07-09 17:42:03 +0700

    set up to delo

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
 from detect import detect_image
+from flask_cors import CORS
 import os
-
+    
 app = Flask(__name__)
+CORS(app) 
 
 @app.route('/')
 def home():
